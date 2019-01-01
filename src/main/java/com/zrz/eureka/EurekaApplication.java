d819922539d51aa5fdbd4ef279740dc919fba59e
zhouruizhong <528441592@qq.com>
2019-01-01 15:27:16 +0800

    init commit

---
+++ src/main/java/com/zrz/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.zrz.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *  服务注册与发现
+ * @author 周瑞忠
+ * @date 2019/1/1 14:47
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
